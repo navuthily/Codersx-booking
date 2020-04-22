5fc14287eb115ecadee72483e01352f743efa836
lyna <vuthilyna304@gmail.com>
2020-04-22 22:21:24 +0700

    commit all files

--- index.js
+++ index.js
@@ -1,8 +1,82 @@
+// server.js
+// where your node app starts
 
-const express = require('express')
-const app = express()
-const port = 3000
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const app = express();
+const port=4000;
+const bodyParser = require("body-parser");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+// Set some defaults
+db.defaults({ books: []}).write();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static("public"));
+app.use(express.static("files"));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.set("view engine", "pug");
+app.set("views", "./views/");
+const methodOverride = require("method-override");
+const shortid = require("shortid");
+var books = db.get("books");
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(
+  methodOverride(req => {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      const method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+app.get("/", (request, response) => {
+  response.send("I love CodersX");
+});
+app.get("/books", function(req, res) {
+  res.render("index", { books: books.value() });
+});
+app.get("/search", function(req, res) {
+  var q = req.query.q;
+  var matched = books.value().filter(function(book) {
+    return book.title.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  });
+  res.render("index", { books: matched });
+});
+app.get("/create", function(req, res) {
+  res.render("create");
+});
+app.post("/create", function(req, res) {
+  req.body.id = shortid.generate();
+
+  books.push(req.body).write();
+  return res.redirect("/books");
+});
+app.get("/view/:id", function(req, res) {
+  var id = req.params.id;
+  var book = books.find({ id: id }).value();
+  return res.render("view", { book });
+});
+app.delete("/delete/:id", function(req, res) {
+  var id = req.params.id;
+  books
+    .remove({ id: id })
+    .write();
+  return res.redirect("/books");
+});
+app.put('/edit/:id', function(req,res){
+  var id=req.params.id;
+  books
+  .find({ id:id})
+  .assign({ title: req.body})
+  .write();
+  return res.redirect("/books");
+})
+// listen for requests :)
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
