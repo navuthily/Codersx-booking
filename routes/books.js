f0c02ea4e6c17021276cbed63e6c2b33452b6b47
lyna <vuthilyna304@gmail.com>
2020-05-10 14:40:14 +0700

    add count cart

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router();
+const {userAuth,userIsNotAuth}=require('../middleware/auth.middleware')
 const{
   getBook,
   create,
@@ -13,11 +14,13 @@ const{
 const {
   uploadMulter,
 } = require('../models/multer');
+
+const {isAdmin,isNotAdmin}=require('../middleware/isAdmin.middleware')
 router.get("/",getBook);
 router.post('/oke',create)
 router.get("/search", getSearch);
-router.get("/create", getCreate);
-router.post("/create",uploadMulter.single('cover'), postCreate);
+router.get("/create", userAuth,isAdmin,getCreate);
+router.post("/create",userAuth,isAdmin,uploadMulter.single('cover'), postCreate);
 router.get("/view/:id", viewDetailBook);
 router.delete("/delete/:id", deleteBook);
 router.post('/edit/:id',uploadMulter.single('cover'), editBook)
